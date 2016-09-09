387fe083de51e19ab3991ee9e3d43ac14d23b439
gml36 <gml36@pitt.edu>
2016-09-09 18:09:31 -0400

    I added the template for submission and review

---
+++ Ch2-Ex3/Ch2-Ex5.py
@@ -0,0 +1,7 @@
+# name: Genevieve Laymon
+# email: gml36@pitt.edu
+# date: 9-9-2016
+# class: CS0008-f2016
+# instructor: Max Novelli (man8@pitt.edu)
+# description: Starting with Python, Chapter 2, Exercise 5
+# notes:
