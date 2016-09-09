597f524fe404e3a8237aec1b0a3f81fe67fc2d4c
gml36 <gml36@pitt.edu>
2016-09-09 18:25:06 -0400

    I added the input for gallons of gas and miles driven.

--- Ch2-Ex3/Ch2-Ex7.py
+++ Ch2-Ex3/Ch2-Ex7.py
@@ -5,3 +5,6 @@
 # instructor: Max Novelli (man8@pitt.edu)
 # description: Starting with Python, Chapter 2, Exercise 5
 # notes:
+
+miles = input('Enter the number of miles driven, please: ')
+gallons = input('Enter the number of gallons of gas used, please: ')
