16b6649b286266710d5d73a29fec7327dd84214e
gml36 <gml36@pitt.edu>
2016-09-09 18:02:08 -0400

    I wrote and finished the exercise

--- Ch2-Ex3/Ch2-Ex3.py
+++ Ch2-Ex3/Ch2-Ex3.py
@@ -6,3 +6,8 @@
 # description: Starting with Python Chapter 2 exercise 3
 # notes:
 
+sq_m = input('Enter the total square meters of land, please: ')
+sq_m = float(sq_m)
+
+acres = sq_m / 4046.8564224
+print 'The total number of acres is ' + str(acres)
