bd8c540c01a43ac2fde270f33644517b10de18b9
gml36 <gml36@pitt.edu>
2016-09-15 13:07:22 -0400

    I wrote and finished the exercise

---
+++ Ex-9-15-2016/Ex-9-15-2016.py
@@ -0,0 +1,8 @@
+num = input ('Please enter your number: ')
+num = float (num)
+if (num > 0):
+    print (num * 3)
+elif (num < 0):
+    print (num + 5)
+elif (num == 0):
+    print 'What a cool number!'
