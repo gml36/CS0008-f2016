93718a02d240d1d9f36fc700e990dab9297c370e
gml36 <gml36@pitt.edu>
2016-09-29 19:25:05 -0400

    corrected error in USC/Metric input

--- f2016_cs8_gml36_a1/f2016_cs8_gml36_a1.py
+++ f2016_cs8_gml36_a1/f2016_cs8_gml36_a1.py
@@ -1,4 +1,4 @@
-system = input('Do you want to use Metric?')
+system = input('Do you want to use USC or Metric?')
 #here I am asking if the client wants Metric or USC
 distance = float(input ('What was the distance driven?'))
 #here I am asking for the distance the client drove
