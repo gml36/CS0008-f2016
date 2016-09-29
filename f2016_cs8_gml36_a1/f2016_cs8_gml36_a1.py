9848dccbf06e0b618f3e51394b98db11fb69d9f2
gml36 <gml36@pitt.edu>
2016-09-28 23:49:04 -0400

    I added the input asking the amount of gasoline and comments

--- f2016_cs8_gml36_a1/f2016_cs8_gml36_a1.py
+++ f2016_cs8_gml36_a1/f2016_cs8_gml36_a1.py
@@ -2,3 +2,5 @@ system = input('Do you want to use Metric?')
 #here I am asking if the client wants Metric or USC
 distance = float(input ('What was the distance driven?'))
 #here I am asking for the distance the client drove
+amount = float(input ('How much gasoline was used?'))
+#here I am asking for the amount of gas the client used
