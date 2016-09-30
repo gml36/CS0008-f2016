4f47b0ccd432ebd5623fc8d6415b3f94d0534c70
gml36 <gml36@pitt.edu>
2016-09-30 16:18:37 -0400

    calculated metric vs usc distances

--- f2016_cs8_gml36_a1/f2016_cs8_gml36_a1.py
+++ f2016_cs8_gml36_a1/f2016_cs8_gml36_a1.py
@@ -7,3 +7,15 @@ distance = float(input('What was the distance driven?'))
 # here I am asking for the distance the client drove
 amount = float(input('How much gasoline was used?'))
 # here I am asking for the amount of gas the client used
+
+metric_distance = 0
+usc_distance = 0
+if system == 'USC':
+    usc_distance = distance
+    # TODO check conversion
+    metric_distance = usc_distance*0.621371
+else:
+    metric_distance = distance
+    #TODO check conversion
+    usc_distance = metric_distance*1.60934
+# calculated metric vs usc distance
