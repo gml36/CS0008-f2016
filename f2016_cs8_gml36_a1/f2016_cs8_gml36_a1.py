89ae184bc79cf22a5da67babb541230a8d044b8c
gml36 <gml36@pitt.edu>
2016-09-30 16:52:49 -0400

    corrected conversions

--- f2016_cs8_gml36_a1/f2016_cs8_gml36_a1.py
+++ f2016_cs8_gml36_a1/f2016_cs8_gml36_a1.py
@@ -1,33 +1,31 @@
-system = input('Do you want to use USC or Metric?')
+system = raw_input('Do you want to use USC or Metric?')
 # here I am asking if the client wants Metric or USC
 if system != 'USC' and system != 'Metric':
     input('System not recognized! Press enter to exit.')
     exit()
-distance = float(input('What was the distance driven?'))
+distance = float(raw_input('What was the distance driven?'))
 # here I am asking for the distance the client drove
-amount = float(input('How much gasoline was used?'))
+amount = float(raw_input('How much gasoline was used?'))
 # here I am asking for the amount of gas the client used
 
 metric_distance = 0
 usc_distance = 0
 if system == 'USC':
     usc_distance = distance
-    # TODO check conversion
-    metric_distance = usc_distance*0.621371
+    metric_distance = usc_distance*1.60934
 else:
     metric_distance = distance
-    #TODO check conversion
-    usc_distance = metric_distance*1.60934
+    usc_distance = metric_distance*0.621371
 # calculated metric vs usc distance
 
 gallons = 0
 liters = 0
 if system == 'USC':
     gallons = amount
-    liters = gallons*0.264172
+    liters = gallons*3.78541
 else:
     liters = amount
-    gallons = liters*3.78541
+    gallons = liters*0.264172
 # calculated gallons and liters
 mpg = usc_distance/gallons
 # calculated mpg
@@ -46,3 +44,5 @@ elif metric_consumption > 8 and metric_consumption <= 10:
 else:
     rating = 'Excellent'
 # calculated ratings for fuel consumption
+
+
