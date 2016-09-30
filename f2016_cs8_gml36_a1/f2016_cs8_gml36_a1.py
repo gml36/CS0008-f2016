c035ef63e3266fc469d7739acac2f8a0cd957b11
gml36 <gml36@pitt.edu>
2016-09-30 16:05:44 -0400

    check to make sure user entered valid system

--- f2016_cs8_gml36_a1/f2016_cs8_gml36_a1.py
+++ f2016_cs8_gml36_a1/f2016_cs8_gml36_a1.py
@@ -1,6 +1,9 @@
 system = input('Do you want to use USC or Metric?')
-#here I am asking if the client wants Metric or USC
-distance = float(input ('What was the distance driven?'))
-#here I am asking for the distance the client drove
-amount = float(input ('How much gasoline was used?'))
-#here I am asking for the amount of gas the client used
+# here I am asking if the client wants Metric or USC
+if system != 'USC' and system != 'Metric':
+    input('System not recognized! Press enter to exit.')
+    exit()
+distance = float(input('What was the distance driven?'))
+# here I am asking for the distance the client drove
+amount = float(input('How much gasoline was used?'))
+# here I am asking for the amount of gas the client used
