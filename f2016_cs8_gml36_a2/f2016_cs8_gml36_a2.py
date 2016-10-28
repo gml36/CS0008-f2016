e781bd0ac3a2ddc84565b4f4c800c7b3387e34de
gml36 <gml36@pitt.edu>
2016-10-28 17:06:43 -0400

    added printKV

--- f2016_cs8_gml36_a2/f2016_cs8_gml36_a2.py
+++ f2016_cs8_gml36_a2/f2016_cs8_gml36_a2.py
@@ -22,3 +22,21 @@ def processFile(fh):
         total_distance += float(distance)
     # returns the number of lines and the total distance
     return [num_lines, total_distance]
+
+# this prints a key and a value with consistent formatting
+def printKV(key,value,klen = 0):
+    # figures out how many spaces to give the key
+    max_len = max(len(key), klen)
+    if isinstance(value, str):
+        # formats and prints string values
+        format_str = '{:' + str(max_len) + '}:{:20)'
+        print(format_str.format(key, value))
+    elif isinstance(value, float):
+        #formats and prints float values
+        format_str = '{:' + str(max_len) + '}:{:6.3)'
+        print(format_str.format(key, value))
+    else:
+        # formats and prints all other values, including ints
+        format_str = '{:' + str(max_len) + '}:{:10)'
+        print(format_str.format(key, value))
+
