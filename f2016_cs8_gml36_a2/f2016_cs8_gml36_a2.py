b96b0693f308c5128b842d4aa63d55de468bd3fc
gml36 <gml36@pitt.edu>
2016-10-28 16:36:15 -0400

    added process file

--- f2016_cs8_gml36_a2/f2016_cs8_gml36_a2.py
+++ f2016_cs8_gml36_a2/f2016_cs8_gml36_a2.py
@@ -4,3 +4,21 @@
 # class: CS0008-f2016
 # instructor: Max Novelli (man8@pitt.edu)
 # description: Assignment 2
+
+# this function takes the handle to a file object
+# and returns the number of lines and the total distance
+def processFile(fh):
+    # here I am defining variables
+    num_lines = 0
+    total_distance = 0
+    # loop through each line in the file
+    for line in fh:
+        # count this line
+        num_lines += 1
+        # gets the distance and adds it to the total
+        line = line.rstrip('\n')
+        line = line.split(',')
+        distance = line[1]
+        total_distance += float(distance)
+    # returns the number of lines and the total distance
+    return [num_lines, total_distance]
