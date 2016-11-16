d3bc11160299a6cbc3c20a6931f3a2e56fb6a209
gml36 <gml36@pitt.edu>
2016-11-15 22:37:50 -0500

    added comments

--- f2016_cs8_gml36_a3/f2016_cs8_gml36_a3.py
+++ f2016_cs8_gml36_a3/f2016_cs8_gml36_a3.py
@@ -1,17 +1,31 @@
+# name: Genevieve Laymon
+# email: gml36@pitt.edu
+# date: 11-15-2016
+# class: CS0008-f2016
+# instructor: Max Novelli (man8@pitt.edu)
+# description: Assignment 3
+
 import sys
+# needed to get max and min values
+
 
 def parse_file_list(file_list):
+# open all of the data files
     open_files = []
     for data in file_list:
         open_files.append(open(data))
     return open_files
 
 def read_data_files(data_files):
+# read the data files and make a dictionary
     running_distances = {'num_lines_read': 0}
+    # I'm also storing the number of lines read here
+    # This could be a problem if someone was named "num_lines_read", but that probably won't happen
     for data_file in data_files:
         for line in data_file:
             running_distances['num_lines_read'] += 1
             runner_info = line.split(',')
+            # info is stored like "name: [distance run, number of records]"
             if runner_info[0] in running_distances:
                 (running_distances[runner_info[0]])[0] += float(runner_info[1])
                 (running_distances[runner_info[0]])[1] += 1
@@ -24,8 +38,10 @@ master_data_file = open(master_data_name, 'r')
 data_file_list = []
 for line in master_data_file:
     data_file_list.append(line)
+# get the master data file and read the names of the individual data files
 data_files = parse_file_list(data_file_list)
 master_data_file.close()
+# close the master data file
 runner_info = read_data_files(data_files)
 num_files_read = len(data_files)
 num_lines_read = runner_info['num_lines_read']
@@ -34,6 +50,7 @@ for item in data_files:
     item.close()
 
 current_max = -sys.maxsize - 1
+# getting the maximum
 max_name = ''
 for name, distance in runner_info:
     if distance[0] > current_max:
@@ -41,6 +58,7 @@ for name, distance in runner_info:
         max_name = name
 
 current_min = sys.maxsize
+# getting the minimum
 min_name = ''
 for name, distance in runner_info:
     if distance[0] < current_min:
@@ -48,11 +66,13 @@ for name, distance in runner_info:
         min_name = name
 
 multiple_records = 0
+# figuring out who has multiple records
 for runner in runner_info:
     if runner[1] > 1:
         multiple_records += 1
 
 total_dist_run = 0
+# calculating the total distance run
 for runner in runner_info:
     total_dist_run += runner[0]
 
@@ -65,3 +85,4 @@ print('Min distance run: ' + str(current_min))
 print('By participant: ' + str(min_name))
 print('Total number of participants: ' + str(len(runner_info)))
 print('Number of participants with multiple records: ' + str(multiple_records))
+# printing the output
