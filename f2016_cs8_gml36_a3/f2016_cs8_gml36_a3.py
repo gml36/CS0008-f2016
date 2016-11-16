6b13dfb15eda7b5ca95db93b4c1c62f289b33b4a
gml36 <gml36@pitt.edu>
2016-11-15 21:23:21 -0500

    open all of the data files

--- f2016_cs8_gml36_a3/f2016_cs8_gml36_a3.py
+++ f2016_cs8_gml36_a3/f2016_cs8_gml36_a3.py
@@ -1,3 +1,9 @@
+def parse_file_list(file_list):
+    open_files = []
+    for data in file_list:
+        open_files.append(open(data))
+    return open_files
+
 master_data_name = raw_input('Enter the name of the master data file: ')
 master_data_file = open(master_data_name, 'r')
 data_file_list = []
